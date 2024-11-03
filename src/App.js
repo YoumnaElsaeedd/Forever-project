5336f21773f837df52a7dbe7678b1d07df75ede0
youmna <youmnaelsaeed43@gmail.com>
2024-11-03 08:30:27 +0300

    aaaaaaaaaaa

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ import ShopContextProvider from './context/shoppContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AdminPanel from './pages/AdminPanel';
-import Checkout from './pages/checkout'; 
 import Login from './pages/Login';
+import Checkout from './pages/checkout';
 
 
 function AppContent() {
@@ -36,7 +36,7 @@ function AppContent() {
           <Route path='/product/:productId' element={<Details />} />
           <Route path='/cart' element={<Cart/>} />
           <Route path='/login' element={<Login2/>} />
-          <Route path='/Checkout' element={<Checkout/>} />
+          <Route path='/Checkout' element={<Checkout/>}/>
           <Route path='/MyOrders' element={<MyOrders/>} />
           <Route path='/LoginUser' element={<Login/>} />
           <Route path='/AdminPanel/*' element={<AdminPanel />} /> 
