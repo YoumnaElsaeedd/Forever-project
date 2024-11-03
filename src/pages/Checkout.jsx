ffa12697dab7a583aaf9f4e3d781510d401cdd81
youmna <youmnaelsaeed43@gmail.com>
2024-11-03 08:09:39 +0300

    aaaaa

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,69 +1,63 @@
-import React, { useState } from 'react'
-import Title from '../components/Title'
-import CartTotal from '../components/CartTotal'
-import { assets } from '../assets/frontend_assets/assets'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import Title from '../components/Title';
+import CartTotal from '../components/CartTotal';
+import { assets } from '../assets/frontend_assets/assets';
+import { useNavigate } from 'react-router-dom';
 
-const Checkout=() => {
-  const [method,setMethod]=useState('cash');
+const Checkout = () => {
+  const [method, setMethod] = useState('cash');
   const navigate = useNavigate();
-  
+
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
       <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
         <div className='text-xl sm:text-2xl my-3'>
-          <Title  text1={'DELIVERY'} text2={'INFORMATION'}/>
+          <Title text1={'DELIVERY'} text2={'INFORMATION'} />
         </div>
         <div className='flex gap-3'>
-          <input type='text' placeholder='First Name' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
-          <input type='text' placeholder='Last Name' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
+          <input type='text' placeholder='First Name' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+          <input type='text' placeholder='Last Name' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
         </div>
-        <input type='email' placeholder='Email Address' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
-        <input type='text' placeholder='Street' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
+        <input type='email' placeholder='Email Address' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+        <input type='text' placeholder='Street' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
         <div className='flex gap-3'>
-          <input type='text' placeholder='City' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
-          <input type='text' placeholder='State' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
+          <input type='text' placeholder='City' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+          <input type='text' placeholder='State' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
         </div>
         <div className='flex gap-3'>
-          <input type='number' placeholder='Zipcode' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
-          <input type='text' placeholder='Country' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
+          <input type='number' placeholder='Zipcode' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+          <input type='text' placeholder='Country' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
         </div>
-        <input type='number' placeholder='Phone' className='border border-gray-300 rounded py-1.5 px-3.5 w-full'/>
-
+        <input type='number' placeholder='Phone' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
       </div>
       <div className='mt-8'>
         <div className='mt-8 min-w-80'>
-        <CartTotal/>
-      </div>
-      <div className='mt-12'>
-        <div className='flex justify-start'> <Title  text1={'PAYMENT'} text2={'METHOD'}/></div>
-       
-        <div className='flex gap-3 flex-col lg:flex-row'>
-          <div onClick={()=>setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-            <p className={`min-w-3.5 h-3.5 border rounded-full  ${method==='stripe'? 'bg-green-400':" "}`}></p>
-            <img src={assets.stripe_logo} className='h-5 mx-4'/>
+          <CartTotal />
+        </div>
+        <div className='mt-12'>
+          <div className='flex justify-start'> <Title text1={'PAYMENT'} text2={'METHOD'} /></div>
+
+          <div className='flex gap-3 flex-col lg:flex-row'>
+            <div onClick={() => setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : " "}`}></p>
+              <img src={assets.stripe_logo} alt="Stripe payment method" className='h-5 mx-4' />
+            </div>
+            <div onClick={() => setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-400' : " "}`}></p>
+              <img src={assets.razorpay_logo} alt="Razorpay payment method" className='h-5 mx-4' />
+            </div>
+            <div onClick={() => setMethod('cash')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cash' ? 'bg-green-400' : " "}`}></p>
+              <p className='text-gray-500 text-sm font-medium mx-4'>Cash On Delivery</p>
+            </div>
           </div>
-          <div onClick={()=>setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-            <p className={`min-w-3.5 h-3.5 border rounded-full  ${method==='razorpay'? 'bg-green-400':" "}`}></p>
-            <img src={assets.razorpay_logo} className='h-5 mx-4'/>
-          </div>
-          <div onClick={()=>setMethod('cash')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-            <p className={`min-w-3.5 h-3.5 border rounded-full ${method==='cash'? 'bg-green-400':" "}`}></p>
-            <p className='text-gray-500 text-sm font-medium mx-4'>Cash On Delivery</p>
+          <div className='w-full text-end mt-8'>
+            <button onClick={() => navigate('/MyOrders')} className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER </button>
           </div>
         </div>
-        <div className='w-full text-end mt-8'>
-          <button onClick={() => navigate('/MyOrders')} className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER </button>
-            
-        </div>
+      </div>
+    </div>
+  );
+};
 
-      </div>
-      </div>
-      
-      
-    
-      </div>
-  )
-}
-
-export default Checkout
+export default Checkout;
